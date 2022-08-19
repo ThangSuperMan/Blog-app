537d31c9520d93724f7e3749a20fd4c4fa8d0a6b
thangsuperman <thangjenny2002@gmail.com>
2022-08-19 17:03:17 +0700

    Adding the delete session cookie and adding the profil page

--- controller/index.go
+++ controller/index.go
@@ -15,64 +15,205 @@ import (
 	"github.com/google/uuid"
 )
 
-// 7200 miliseconds
-var LIVETIME_COOKIE int = 7200
+type AccessToken struct {
+	IsSignedIn  bool
+	Username    string
+	ProfileName string
+	AvatarName  string
+}
+
+// One hour
+var LIVETIME_COOKIE int = 1
+
+func GetSessionTokenCookie(cookieName string, r *http.Request) string {
+	cookie, err := r.Cookie(cookieName)
+
+	if err != nil {
+		fmt.Println("Error when trying to get the cookie: ", err)
+	}
+	sessionToken := cookie.Value
+	return sessionToken
+}
 
 func CheckSessionCookieExists(w http.ResponseWriter, r *http.Request) bool {
-  fmt.Println("AutoAdaptedAccountAuthenticated")
 	_, err := r.Cookie("my_cookie")
 
 	if err != nil {
 		if err == http.ErrNoCookie {
 			w.WriteHeader(http.StatusUnauthorized)
-			return false 
+			return false
 		}
 		w.WriteHeader(http.StatusBadRequest)
 		return false
 	}
 
-  // Session cookie exist
-  return true
+	// Session cookie exist
+	return true
 }
 
-func RenderHomePage(w http.ResponseWriter, r *http.Request) {
+func LogOut(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("LogOut controller")
+	_, err := r.Cookie("my_cookie")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println("Have cookie, now delete that.")
+	cookieName := "my_cookie"
+	var sessionToken string = GetSessionTokenCookie(cookieName, r)
+	fmt.Println("sessionToken: ", sessionToken)
+	db := model.ConnectDatabase()
+	model.DeleteSessionCookie(db, sessionToken)
+
+	cookie := &http.Cookie{
+		Name:     "my_cookie",
+		Value:    "",
+		Path:     "/",
+		HttpOnly: true,
+	}
+
+	http.SetCookie(w, cookie)
+}
+
+func RenderTemplate(templateName string, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("RenderTemplate")
 	tpl, err := template.ParseGlob("./templates/*.html")
 	helper.HaltOn(err)
 
-	var isAuthenticated bool = CheckSessionCookieExists(w, r)
+	var cookieExists bool = CheckSessionCookieExists(w, r)
+	cookieName := "my_cookie"
 
-  if isAuthenticated {
-    fmt.Println("isAuthenticated: ", isAuthenticated)
-    cookieName := "my_cookie"
-    var sessionToken string = GetSessionCookie(cookieName, r)
-    fmt.Println("sessionToken: ", sessionToken )
-  }
+	if cookieExists {
+		var sessionToken string = GetSessionTokenCookie(cookieName, r)
+		if sessionToken != "" {
+			db := model.ConnectDatabase()
+			var idUser int = model.GetIdUserFromSessionsTable(db, sessionToken)
+			var user model.User
+			user = model.GetInfoUser(db, idUser)
+			data := AccessToken{
+				IsSignedIn:  true,
+				Username:    user.Username,
+				ProfileName: user.Profile_name,
+				AvatarName:  user.Avatar_name,
+			}
 
-  tpl.ExecuteTemplate(w, "index.html", nil)
-  
+			tpl.ExecuteTemplate(w, templateName, data)
+		} else {
+			tpl.ExecuteTemplate(w, templateName, nil)
+		}
+	} else {
+		tpl.ExecuteTemplate(w, templateName, nil)
+	}
+}
 
-	// if isAuthenticate {
-	// 	cookie, _ := r.Cookie("my_cookie")
-	// 	sessionsToken := cookie.Value
- //    // db := model.ConnectDatabase()
- //    // model.GetInfoUser(db)
-	// 	accessToken := AccessToken{
-	// 		IsSignedIn: true,
-	// 		Username:   sessions[sessionsToken].username,
+func RenderHomePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("RenderHomePage")
+	templateName := "index.html"
+	RenderTemplate(templateName, w, r)
+	// tpl, err := template.ParseGlob("./templates/*.html")
+	// helper.HaltOn(err)
+	//
+	// var cookieExists bool = CheckSessionCookieExists(w, r)
+	// cookieName := "my_cookie"
+	//
+	// if cookieExists {
+	// 	var sessionToken string = GetSessionTokenCookie(cookieName, r)
+	// 	if sessionToken != "" {
+	// 		db := model.ConnectDatabase()
+	// 		var idUser int = model.GetIdUserFromSessionsTable(db, sessionToken)
+	// 		var user model.User
+	// 		user = model.GetInfoUser(db, idUser)
+	// 		data := AccessToken{
+	// 			IsSignedIn:  true,
+	// 			Username:    user.Username,
+	// 			ProfileName: user.Profile_name,
+	// 			AvatarName:  user.Avatar_name,
+	// 		}
+	//
+	// 		tpl.ExecuteTemplate(w, "index.html", data)
+	// 	} else {
+	// 		tpl.ExecuteTemplate(w, "index.html", nil)
 	// 	}
- //
-	// 	tpl.ExecuteTemplate(w, "index.html", accessToken)
 	// } else {
 	// 	tpl.ExecuteTemplate(w, "index.html", nil)
 	// }
 }
 
-func RenderSignUpPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("RenderSignUpPage")
+func RenderProfilePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("RenderProfilePage")
 	tpl, err := template.ParseGlob("./templates/*.html")
-
 	helper.HaltOn(err)
-	tpl.ExecuteTemplate(w, "signup.html", nil)
+	var cookieExists bool = CheckSessionCookieExists(w, r)
+
+	cookieName := "my_cookie"
+
+	if cookieExists {
+		var sessionToken string = GetSessionTokenCookie(cookieName, r)
+		if sessionToken != "" {
+			db := model.ConnectDatabase()
+			var idUser int = model.GetIdUserFromSessionsTable(db, sessionToken)
+			// username, avatarName := model.GetUsernameAndAvatarNameOfUsersTable(db, idUser)
+
+			var user model.User
+			user = model.GetInfoUser(db, idUser)
+			fmt.Println("user: ", user)
+
+			data := AccessToken{
+				IsSignedIn:  true,
+				Username:    user.Username,
+				ProfileName: user.Profile_name,
+				AvatarName:  user.Avatar_name,
+			}
+
+			tpl.ExecuteTemplate(w, "profile.html", data)
+		} else {
+			tpl.ExecuteTemplate(w, "profile.html", nil)
+		}
+	} else {
+		tpl.ExecuteTemplate(w, "profile.html", nil)
+	}
+}
+
+func RenderAboutPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("RenderAboutPage")
+	templateName := "about.html"
+	RenderTemplate(templateName, w, r)
+	// tpl, err := template.ParseGlob("./templates/*.html")
+	// helper.HaltOn(err)
+	// var cookieExists bool = CheckSessionCookieExists(w, r)
+	//
+	// cookieName := "my_cookie"
+	//
+	// if cookieExists {
+	// 	var sessionToken string = GetSessionTokenCookie(cookieName, r)
+	// 	if sessionToken != "" {
+	// 		db := model.ConnectDatabase()
+	// 		var idUser int = model.GetIdUserFromSessionsTable(db, sessionToken)
+	// 		// username, avatarName := model.GetUsernameAndAvatarNameOfUsersTable(db, idUser)
+	//
+	// 		var user model.User
+	// 		user = model.GetInfoUser(db, idUser)
+	// 		fmt.Println("user: ", user)
+	//
+	// 		data := AccessToken{
+	// 			IsSignedIn:  true,
+	// 			Username:    user.Username,
+	// 			ProfileName: user.Profile_name,
+	// 			AvatarName:  user.Avatar_name,
+	// 		}
+	//
+	// 		tpl.ExecuteTemplate(w, "about.html", data)
+	// 	} else {
+	// 		tpl.ExecuteTemplate(w, "about.html", nil)
+	// 	}
+	// } else {
+	// 	tpl.ExecuteTemplate(w, "about.html", nil)
+	// }
 }
 
 func HandlerSignup(w http.ResponseWriter, r *http.Request) {
@@ -86,15 +227,21 @@ func HandlerSignup(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Post method
 	r.ParseForm()
 	r.ParseMultipartForm(10)
 
 	username := r.FormValue("username")
 	password := r.FormValue("password")
+	profileName := r.FormValue("profile_name")
 	confirmPassword := r.FormValue("confirm_password")
 	createdAt := time.Now().Format("2006-01-02 15:04:05")
 	updatedAt := ""
 
+	fmt.Println("username: ", username)
+	fmt.Println("password: ", password)
+	fmt.Println("avatarProfile: ", profileName)
+	fmt.Println("confirmPassword: ", confirmPassword)
 	file, fileHeader, e := r.FormFile("avatar_profile")
 	helper.HaltOn(e)
 	defer file.Close()
@@ -106,11 +253,11 @@ func HandlerSignup(w http.ResponseWriter, r *http.Request) {
 	var avatarName string
 
 	if contentType == "image/jpeg" {
-		osFile, err = ioutil.TempFile("uploads/images", "*.jpg")
-		avatarName = strings.TrimLeft(osFile.Name(), "uploads/images/")
+		osFile, err = ioutil.TempFile("static/uploads/images", "*.jpg")
+		avatarName = strings.TrimLeft(osFile.Name(), "static/uploads/images")
 	} else if contentType == "image/png" {
-		osFile, err = ioutil.TempFile("uploads/images", "*.png")
-		avatarName = strings.TrimLeft(osFile.Name(), "uploads/images/")
+		osFile, err = ioutil.TempFile("static/uploads/images", "*.png")
+		avatarName = strings.TrimLeft(osFile.Name(), "static/uploads/images/")
 	}
 
 	defer osFile.Close()
@@ -123,7 +270,7 @@ func HandlerSignup(w http.ResponseWriter, r *http.Request) {
 
 	if password == confirmPassword {
 		db := model.ConnectDatabase()
-		model.AddUser(db, username, password, avatarName, createdAt, updatedAt)
+		model.AddUser(db, username, password, profileName, avatarName, createdAt, updatedAt)
 		fmt.Fprintln(w, "Successfully signup have fun and ejoy.")
 	}
 }
@@ -134,9 +281,10 @@ func HandleSignIn(w http.ResponseWriter, r *http.Request) {
 		tpl, err := template.ParseGlob("./templates/*.html")
 		helper.HaltOn(err)
 		tpl.ExecuteTemplate(w, "signin.html", nil)
-    return
+		return
 	}
 
+	fmt.Println("r.Method: ", r.Method)
 	e := r.ParseForm()
 	if e != nil {
 		log.Fatal(e)
@@ -144,31 +292,41 @@ func HandleSignIn(w http.ResponseWriter, r *http.Request) {
 
 	username := r.FormValue("username")
 	password := r.FormValue("password")
-  db := model.ConnectDatabase()
-  usernameModel, passwordModel := model.GetInfoUser(db, username, password)
+	db := model.ConnectDatabase()
+	var idUser int = model.GetIdUser(db, username)
 
-  if usernameModel != "" && passwordModel != ""{
-    if username == usernameModel && password == passwordModel {
-      fmt.Println("username == usernameModel && password == passwordModel")
-      var idUser int = model.GetIdUser(db, username)  
-      fmt.Println("idUser: ", idUser)
+	model.GetInfoUser(db, idUser)
 
-      var sessionToken string = uuid.NewString()
+	usernameModel, passwordModel := model.GetUsernameAndPasswordOfUser(db, username)
+	fmt.Println("usernameModel: ", usernameModel)
+	fmt.Println("passwordModel: ", passwordModel)
 
-      expriesAt := time.Now().Add(time.Duration(LIVETIME_COOKIE) * time.Second)
-      model.AddSession(db, sessionToken, idUser)
+	if usernameModel != "" && passwordModel != "" {
+		fmt.Println("if statement")
+		if username == usernameModel && password == passwordModel {
+			var idUser int = model.GetIdUser(db, username)
+			var sessionToken string = uuid.NewString()
 
-      http.SetCookie(w, &http.Cookie {
-        Name: "my_cookie",
-        Value: sessionToken,
-        Expires: expriesAt,
-      })
+			expiry := time.Now().Add(1 * time.Hour)
+			createdAt := time.Now().Format("2006-01-02 15:04:05")
+			fmt.Println("username == usernameModel && password == passwordModel")
+			fmt.Println("createdAt: ", createdAt)
+			fmt.Println("expriesAt: ", expiry)
+			model.AddSession(db, sessionToken, expiry, createdAt, idUser)
 
-      fmt.Fprintln(w, "<p>Successfully signup, now have fun and enjoy.</p>")
-    } else {
-      fmt.Println("The info user you just type had somthing wrong!")
-    }
-  }
+			http.SetCookie(w, &http.Cookie{
+				Name:    "my_cookie",
+				Value:   sessionToken,
+				Expires: expiry,
+			})
 
-
+			fmt.Fprintln(w, "<p>Successfully signin, now have fun and enjoy.</p>")
+		} else if username == usernameModel && password != passwordModel {
+			fmt.Fprintln(w, "<p>Username exist but the password was wrong, please make sure that you was typing a correct one.</p>")
+		}
+	} else {
+		fmt.Println("else case")
+		fmt.Println("Sorry but the username you just typed is not exist, please make sure you was typing the correct username.")
+		fmt.Fprintln(w, "<p>Sorry but the username you just typed is not exist, please make sure you was typing the correct username.</p>")
+	}
 }
--- model/index.go
+++ model/index.go
@@ -3,32 +3,38 @@ package model
 import (
 	"database/sql"
 	"fmt"
+	"time"
+
 	_ "github.com/mattn/go-sqlite3"
 )
 
-type user struct {
-	Id          int
-	Username    string
-	Password    string
-	avatar_name string
-	created_at  string
-	updated_at  string
+type User struct {
+	Id_user      int
+	Username     string
+	Password     string
+	Profile_name string
+	Avatar_name  string
+	Created_at   string
+	Updated_at   string
 }
 
 func createTables(db *sql.DB) {
 	var statement string = `
 		create table if not exists "users" (
 		"id_user"     integer not null primary key autoincrement,
-		"username"	  text not null,
-		"password"	  text not null,
-    "avatar_name" text not null,
-		"created_at"  text not null,
-    "updated_at"  text
+    "username"	    text not null,
+		"password"	    text not null,
+		"profile_name"  text not null,
+    "avatar_name"   text not null,
+		"created_at"    text not null,
+    "updated_at"    text
 	);
 
     create table if not exists "sessions" (
-    "id_session" integer not null primary key autoincrement,
+    "id_session"    integer not null primary key autoincrement,
     "session_token" text not null,
+    "created_at"    text not null,
+    "expiry"        datetime not null, 
     "id_user" integer not null,
     foreign key (id_user) references users (id_user)
     )
@@ -41,7 +47,6 @@ func createTables(db *sql.DB) {
 }
 
 func InitModel() {
-	fmt.Println("InitModel")
 	sqliteDatabase, _ := sql.Open("sqlite3", "./my_database.db")
 
 	defer sqliteDatabase.Close()
@@ -53,8 +58,15 @@ func ConnectDatabase() *sql.DB {
 	return sqliteDatabase
 }
 
+func DeleteSessionCookie(db *sql.DB, sessionToken string) {
+	fmt.Println("DeleteSessionCookie: ", sessionToken)
+	statement := `delete from sessions where session_token=?`
+	stmt, _ := db.Prepare(statement)
+	result, _ := stmt.Exec(sessionToken)
+	fmt.Println("Result when delete session cookie inside the sesions table: ", result)
+}
+
 func ReadSessionCookie(db *sql.DB, sessionTokenParams string) (string, int) {
-	fmt.Println("model ReadSession")
 	statement := `select session_token, id_user from sessions where session_token=?`
 	var sessionToken string
 	var idUser int
@@ -70,24 +82,62 @@ func ReadSessionCookie(db *sql.DB, sessionTokenParams string) (string, int) {
 	return sessionToken, idUser
 }
 
-func AddSession(db *sql.DB, sessionToken string, idUser int) {
-	fmt.Println("model AddSession")
-	statment := `insert into sessions(session_token, id_user) values (?, ?)`
-
-	stmt, _ := db.Prepare(statment)
-	result, _ := stmt.Exec(sessionToken, idUser)
+func AddSession(db *sql.DB, sessionToken string, expriy time.Time, createdAt string, idUser int) {
+	statement := `insert into sessions(session_token, expiry, created_at, id_user) values (?, ?, ?, ?)`
+	stmt, _ := db.Prepare(statement)
+	result, _ := stmt.Exec(sessionToken, expriy, createdAt, idUser)
 	fmt.Println("result when add session record: ", result)
 }
 
-func GetInfoUser(db *sql.DB, usernameParams string, passwordParams string) (string, string) {
-	fmt.Println("validateUser")
+func GetInfoUser(db *sql.DB, idUserParams int) User {
+	var idUser int
+	var username string
+	var password string
+	var profileName string
+	var avatarName string
+
+	statement := `select id_user, username, password, profile_name, avatar_name from users where id_user=?`
+	row := db.QueryRow(statement, idUserParams)
+	err := row.Scan(&idUser, &username, &password, &profileName, &avatarName)
+
+	if err != nil {
+		fmt.Println("Error when scan info user record: ", err)
+		return User{}
+	}
+
+	user := User{
+		Id_user:      idUser,
+		Username:     username,
+		Password:     password,
+		Profile_name: profileName,
+		Avatar_name:  avatarName,
+	}
+
+	return user
+}
+
+func GetUsernameAndAvatarNameOfUsersTable(db *sql.DB, idUser int) (string, string) {
+	var username string
+	var avatarName string
+	statement := `select username, avatar_name from users where id_user=?`
+	row := db.QueryRow(statement, idUser)
+	err := row.Scan(&username, &avatarName)
+
+	if err != nil {
+		fmt.Println("Error when scan info user record: ", err)
+		return "", ""
+	}
+
+	return username, avatarName
+}
+
+func GetUsernameAndPasswordOfUser(db *sql.DB, usernameParams string) (string, string) {
 	var username string
 	var password string
 	statement := `select username, password from users where username=?`
-
-	row := db.QueryRow(statement, usernameParams, passwordParams)
-
+	row := db.QueryRow(statement, usernameParams)
 	err := row.Scan(&username, &password)
+
 	if err != nil {
 		fmt.Println("Error when scan info user record: ", err)
 		return "", ""
@@ -96,8 +146,20 @@ func GetInfoUser(db *sql.DB, usernameParams string, passwordParams string) (stri
 	return username, password
 }
 
+func GetIdUserFromSessionsTable(db *sql.DB, sessionToken string) int {
+	var idUser int
+	statement := `select id_user from sessions where session_token=?`
+	row := db.QueryRow(statement, sessionToken)
+	err := row.Scan(&idUser)
+
+	if err != nil {
+		fmt.Println("Error when scan info user record: ", err)
+	}
+
+	return idUser
+}
+
 func GetIdUser(db *sql.DB, username string) int {
-	fmt.Println("GetIdUser")
 	var idUser int
 	statement := `select id_user from users where username=?`
 
@@ -112,11 +174,10 @@ func GetIdUser(db *sql.DB, username string) int {
 	return idUser
 }
 
-func AddUser(db *sql.DB, username string, password string, avatarName string, createdAt string, updatedAt string) {
-	fmt.Println("model AddUser")
-	statment := `insert into users(username,  password, avatar_name, created_at) values (?, ?, ?, ?)`
+func AddUser(db *sql.DB, username string, password string, profileName string, avatarName string, createdAt string, updatedAt string) {
+	statment := `insert into users(username,  password, profile_name, avatar_name, created_at) values (?, ?, ?, ?, ?)`
 
 	stmt, _ := db.Prepare(statment)
-	result, _ := stmt.Exec(username, password, avatarName, createdAt)
-	fmt.Println("result", result)
+	result, _ := stmt.Exec(username, password, profileName, avatarName, createdAt)
+	fmt.Println("result add user record: ", result)
 }
